d67753902a7e9ae70ee2ed302c6a7d791ca4081a
Esteban <4801459559Ee>
2020-10-24 02:27:35 -0400

    added half of assignment 2

--- CS1/ProgrammingAssignment2/cop-3502-as2-LauKoo-Esteban.c
+++ CS1/ProgrammingAssignment2/cop-3502-as2-LauKoo-Esteban.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "leak_detector_c.h"
+#include "leak_detector_c.h"
 #include <math.h>
 
 
 
 struct fish_struct {
-    char *name;
+    int id;
     struct fish_struct *next;
     struct fish_struct *prev;
 };
 
 typedef struct fish_struct fish;
 
-// build the pond struct read values from input.
-struct pond_struct {
-    char *name;
-
-}
-
-
 struct fish_list_struct {
+    int n;
+    int e;
+    int th;
     fish *head;
     fish *tail;
 };
 
 typedef struct fish_list_struct fish_list;
+
+
+// build the pond struct read values from input.
+struct pond_struct {
+    int pond_number;
+    char *name;
+    fish_list *fl;
+};
+
+typedef struct pond_struct pond;
+
+
+
 
 // This was given
 void remove_crlf(char *s)
@@ -79,38 +88,22 @@ int get_number_of_ponds(FILE *ifp) {
 }
 
 
-fish *new_fish(char* name) {
+pond *new_pond_array(int nponds) {
 
-    fish *f;
+    pond *p = calloc(nponds, sizeof(pond));
 
-    f = malloc(sizeof(fish));
-
-    f->name = strdup(name);
-    f->next = NULL;
-    f->prev = NULL;
-
-    return f;
+    return p;
 }
 
-void dispose_fish(fish *f) {
-    free(f->name);
-    free(f);
-}
+void clear_pond(pond *p ) {
 
-void print_fish(fish *f) {
+    p -> pond_number = 0;
 
-    printf("    Fish: %s\n", f->name);
-
-    if(f->prev == NULL) {
-        printf("    No link back\n");
-    } else {
-        printf("    Link back to: %s\n", f->prev->name);
-    }
-    if(f->next == NULL) {
-        printf("    No link forward\n");
-    } else {
-        printf("    Link forward to: %s\n", f->next->name);
-    }
+    if (p->name != NULL) {
+        free(p->name);
+        p->name = NULL;
+    } 
+    
 }
 
 fish_list *new_fish_list(void) {
@@ -118,9 +111,139 @@ fish_list *new_fish_list(void) {
     fish_list *fl;
 
     fl = malloc(sizeof(fish_list));
+    fl->e = 0;
+    fl->n = 0;
+    fl->th = 0;
     fl->head = NULL;
     fl->tail = NULL;
 }
+
+// grabs the information that is passed through and fills in the corresponding parts of the structure.
+void fill_fish_list(fish_list *fl, int n, int e, int th) {
+
+    fl -> n = n;
+    fl -> e = e;
+    fl -> th = th;
+
+}
+
+void dispose_pond_array(pond *p, int nponds) {
+    
+    // calls the clear monster function to remove the data within the structure
+    for (int i = 0; i < nponds; i++) {
+        clear_pond(p + i);
+    }
+    // frees the memory allocation.
+    free(p);
+}
+
+void fill_pond(pond *p, int pond_number, char *name, fish_list *fl) {
+
+    p -> name = strdup(name);
+    p -> pond_number= pond_number;
+    p -> fl = fl;   
+
+
+}
+
+
+
+/* Read a monster from our file. and directs to a fill function */
+void read_pond(FILE *ifp, pond *p) {
+    
+
+    char name[128];
+
+    char order_string[128];
+    int order;
+
+    char n_string[128];
+    int n;
+
+    char e_string[128];
+    int e;
+
+    char th_string[128];
+    int th;
+
+    fish_list *fl;
+ 
+    // scans the the file for the corresponding values and names them accordingly
+    fscanf(ifp, "%s %s %s %s %s", order_string, name, n_string, e_string, th_string);
+
+    order = atoi(order_string);
+
+    fl = new_fish_list();
+
+    n = atoi(n_string);    
+    e = atoi(e_string);
+    th = atoi(th_string);    
+
+    fill_fish_list(fl , n, e, th);
+
+    // sends the information to the fill monster function in order to fill the corresponding data.
+    fill_pond(p , order, name, fl);
+
+}
+
+void print_fish(fish *f) {
+
+    printf("    Fish: %d\n", f->id);
+
+    // if(f->prev == NULL) {
+    //     printf("    No link back\n");
+    // } else {
+    //     printf("    Link back to: %d\n", f->prev->id);
+    // }
+    // if(f->next == NULL) {
+    //     printf("    No link forward\n");
+    // } else {
+    //     printf("    Link forward to: %d\n", f->next->id);
+    // }
+}
+
+// prints the monster based on the given monster structure pointer.
+void print_pond(FILE *ofp, pond *p) {
+
+    int i;
+
+    fprintf(ofp, "%d %s n: %d e: %d th %d Fish: ",
+            p->pond_number, p->name, p->fl->n, p->fl->e, p->fl->th);
+
+
+            
+        fish *f = p->fl->head;
+
+        do {
+            fprintf(ofp, " %d", f->id);
+            f = f->next;
+        } while(f != p->fl->head);    
+
+
+
+
+    fprintf(ofp, "\n");
+
+}
+
+fish *new_fish(int id) {
+
+    fish *f;
+
+    f = malloc(sizeof(fish));
+
+    f->id = id;
+    f->next = NULL;
+    f->prev = NULL;
+
+    return f;
+}
+
+void  dispose_fish(fish *f) {
+
+    free(f);
+}
+
 
 void dispose_fish_list(fish_list *fl) {
 
@@ -144,6 +267,20 @@ void dispose_fish_list_all(fish_list *fl) {
     } while(f != fl->head);
 
     dispose_fish_list(fl);
+}
+
+void print_fish_tail(fish_list *fl) {
+    fish *f = fl->tail;
+
+    if(f == NULL) {
+        return;
+    }
+
+    do {
+        print_fish(f);
+        f = f->prev;
+    } while(f != fl->tail);
+
 }
 
 void print_fish_list(fish_list *fl) {
@@ -257,42 +394,329 @@ void fish_list_delete(fish_list *fl, fish *f_to_delete, int dispose)
     /* Nothing in the list points to us any more, so we can now safely delete. */
 
     clear_links_or_dispose(f_to_delete, dispose);
+
+    // printf("fish: %d deleted\n" , f_to_delete->id);
 }
+
+void output_integer_list(pond *p, int n) {
+
+  for(int i = 0; i < n; i++) {
+    printf("  pond %d\n", p[i].pond_number);
+  }
+  printf("\n");
+}
+
+int find_highest(pond *p, int n, int *comparisons)
+{
+  int highest_val = INT_MIN;
+  int highest_loc = -1;
+  int i;
+  
+  for(i = 0; i <= n; i++)
+  {
+    (*comparisons)++;
+    if((p + i)->pond_number > highest_val)
+    {
+      highest_loc = i;
+      highest_val = p[i].pond_number;
+    }
+  }
+
+  return highest_loc;
+}
+
+int find_highest_fl(pond *p, int n )
+{
+  int highest_val = INT_MIN;
+  fish *highest_position;
+  int i;
+  
+  for(i = 0; i <= n; i++)
+  {
+
+    if(p->fl->head->id > highest_val)
+    {
+      highest_position = p->fl->tail;
+      highest_val =  p->fl->tail->id;
+    }
+  }
+
+  printf("tail: %d\n", p->fl->tail->id);
+
+  return highest_val;
+}
+
+int find_lowest_fl(pond *p, int n)
+{
+
+  fish *lowest_position; 
+  int lowest_val = INT_MAX;
+  int i;
+  
+  printf("n %d\n", n);
+
+  for(i = 0; i <= n; i++)
+  {
+
+    if(p->fl->head->id < lowest_val)
+    {
+
+      lowest_position = p->fl->head;  
+      lowest_val = p->fl->head->id;      
+
+    }
+
+    p->fl->head = p->fl->head->next;
+
+        printf("head value %d\n", p->fl->head->id);
+        printf("lowest val %d\n", lowest_val);
+    
+  }
+
+
+    p->fl->head = lowest_position;
+
+
+    printf("head: %d\n", p->fl->head->id);
+
+
+  return lowest_val;
+}
+
+void selection_sort(pond *p, int n)
+{
+  int i;
+  int highest;
+  pond temp;
+  int comparisons = 0;
+  int swaps = 0;
+
+
+  for(i = n-1; i > 0; i--)
+  {
+    highest = find_highest(p, i, &comparisons);
+    if(highest != i)
+    {
+      swaps++;
+      temp = p[highest];
+      p[highest] = p[i];
+      p[i] = temp;
+    }
+  }
+
+}
+
+// void size_of_linked_list(pond *p) {
+
+//     fish *f;
+
+//     int size = 0;
+//     if (f == NULL) {
+//         printf("HEAD IS NULL");
+//         return;
+//     }
+
+//     f = p->fl->head;
+
+//     while (f != NULL) {
+//         ++size;
+//         f = p->fl->head->next;
+//     }
+
+//     printf("size of list is %d\n", size);
+// }
+
+void eat_fish(pond *p) {
+    
+    int i;
+
+    fish_list *fl = p->fl;
+
+    int n = fl->n;
+    int th = fl->th;
+    
+    // printf("fish e: %d, n: %d, th: %d\n", fl->e, fl->n, fl->th);
+
+    while (n != th) {
+    for (i = 0; i < (fl->e)-1; i++) {
+
+        fl->head = fl->head->next;
+
+
+    }
+        fish *f = fl->head;
+                fish_list_delete(fl, f, 0);
+
+                        --n;
+
+                // printf("n: %d\n", n);
+
+
+    };
+
+
+}
+
+void enqueue(fish_list *fl, fish *f) {
+
+    f->next = NULL;
+
+
+    if(fl->head == NULL) {
+
+    fl->head = f;
+    fl->tail = f;
+
+    } else{
+
+    fl->tail->next = f;
+    fl->tail = f;
+
+    }
+}
+
+    // Add f to the tail of q.
+
+fish *dequeue(fish_list *fl) {
+
+    fish *fish_to_delete = fl->head;
+
+    if ( fl->head != NULL) {
+        fl->head = fl->head->next;
+    }
+
+    if ( fl->head == NULL) {
+        fl->tail= NULL;
+    }
+
+    return fish_to_delete;
+
+}
+
+    // Pull an item off the head of q - don't delete it - and return it.
+
+fish *queue_peek(fish_list *fl) {
+
+    return fl->head;
+
+}
+
+    // Look at the item on the head of q - don't delete it or remove it - and return it.
+
+int is_empty(fish_list *fl) {
+
+    if(queue_peek(fl) == NULL) {
+        return 1;
+    }
+
+    else {
+        return 0;
+    }
+    
+
+}
+
+    // Look to see if q->head == NULL.
+
 
 int main(void) {
 
-    //atexit(report_mem_leak);
+    atexit(report_mem_leak);
 
     FILE *ofp;
     FILE *ifp;
 
-    int i;
+    int i, j, k;
     int nponds;
     pond *ponds;
+
+    fish *f;
+    fish_list *fl;
+
+
 
     ifp = fopen("cop3502-as2-input-2.txt", "r");
     ofp = fopen("cop3502-as2-out-LauKoo-Esteban", "w");
 
     nponds = get_number_of_ponds(ifp);
-    printf("# of ponds = %d\n", nponds);
 
-    ponds = new_ponds_array(nponds);
+
+    ponds = new_pond_array(nponds);
+
+    for (i = 0; i < nponds; i++) {
+        read_pond(ifp, ponds + i );
+    }
+
+    selection_sort(ponds, nponds);
+    // printf("Sorted array: \n"); 
+    // output_integer_list(ponds, nponds);
+
+
+    for (i = 0; i < nponds; i++) {
+
+        for ( j = 0; j < ponds[i].fl->n; j++) {
+            f = new_fish((j+1));
+            fish_list_add_tail(ponds[i].fl, f);
+        }
+    }
+
+    for (i = 0; i < nponds; i++) {
+        print_pond(ofp, ponds + i);
+    }
+
+    for (i = 0; i < nponds; i++) {
+
+  
+        eat_fish((ponds+i));
+
+    }
+
+
+
+    fprintf(ofp,"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+
+
+
+    for (i = 0; i < nponds; i++) {
+
+        int lowest;
+
+        fish *headFL;
+
+        find_lowest_fl(ponds + i, (ponds + i)->fl->th);
+
+        find_highest_fl(ponds + i, (ponds + i)->fl->th);
+
+        printf("---------------------\n");
+
+    }
+
+    //probably need another sort here for the values.
+
+    for (i = 0; i < nponds; i++) {
+        print_pond(ofp, ponds + i);
+    }
+
+    // create formula to place head in the lowest number in each fl
+    // 
+
+
+    // eat_fish((ponds+0));
+    // f = new_fish("failfish1");
+    // fish_list_add(fl, f);
+    // f = new_fish("failfish2");
+    // fish_list_add(fl, f);
+    // f = new_fish("failfish3");
+    // fish_list_add(fl, f);
+
+    // print_fish_list(fl);
+
 
     
+    // dispose_fish_list_all(fl);
 
-    fish *f; 
-    fish_list *fl = new_fish_list();
-
-    f = new_fish("failfish1");
-    fish_list_add(fl, f);
-    f = new_fish("failfish2");
-    fish_list_add(fl, f);
-    f = new_fish("failfish3");
-    fish_list_add(fl, f);
-
-    print_fish_list(fl);
-
-    dispose_fish_list_all(fl);
+    // dispose_pond_array(ponds, nponds);
 
 
     fclose(ifp);
